e725e048675b2a9ab75079d4a36ad7098dd03946
Arvedui <arvedui@posteo.de>
2014-12-03 17:17:50 +0100

    small change for pyinstaller support

--- certifi/core.py
+++ certifi/core.py
@@ -9,11 +9,14 @@ This module returns the installation location of cacert.pem.
 """
 
 import os
+import sys
 
 def where():
-    f = os.path.split(__file__)[0]
-
-    return os.path.join(f, 'cacert.pem')
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, 'cacert.pem')
+    else:
+        f = os.path.split(__file__)[0]
+        return os.path.join(f, 'cacert.pem')
 
 if __name__ == '__main__':
     print(where())
